ea8e35ed0ddb16b0e9bf8ccb418f89baed5bf2bd
Maream Sefan <111729393+Mareamsefan@users.noreply.github.com>
2024-09-15 18:30:04 +0000

    ferdig med oppgave 1

--- oppgave1/index.js
+++ oppgave1/index.js
@@ -5,9 +5,20 @@ const text =
   "Baby cliche unicorn brooklyn farm-to-table. Salvia semiotics hella literally paleo humblebrag bushwick letterpress messenger bag pork belly brooklyn authentic vexillologist. Gastropub sustainable banjo, shaman snackwave viral air plant ramps health goth. Edison bulb vegan microdosing, tote bag unicorn skateboard disrupt copper mug four loko sustainable whatever cloud bread slow-carb lumbersexual four dollar toast. Waistcoat lomo hammock vape shabby chic sartorial yr godard pok pok fashion axe organic migas. Quinoa yr vexillologist intelligentsia verylongwordthatislong neutra mixtape YOLO XOXO listicle letterpress farm-to-table beard.";
 
 const longestWord = () => {
-  // TODO: Gjør om text til array eks. ['Baby', 'cliche']
-  // TODO: Velg første ord så du har noe å sammenlikne med
-  // TODO: Gå igjennom alle ordene og oppdater hvis nytt ord er lengre
+  const textArray = text.split(" ");
+  let longestWordC = " ";
+  console.log(textArray);
+  textArray.forEach((word) => {
+    if (word.length > longestWordC.length) {
+      longestWordC = word;
+      console.log(longestWordC);
+    }
+  });
+  return longestWordC;
 };
 
-console.log(longestWord());
+const result = document.createElement("h1");
+result.textContent = longestWord();
+result.style.color = "pink";
+const body = document.getElementsByTagName("body")[0];
+body.appendChild(result);
